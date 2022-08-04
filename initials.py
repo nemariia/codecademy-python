e5953f583ab75c110de0622b3308f5089c1a4233
nemariia <lusuwey@gmail.com>
2022-08-04 14:31:20 +0300

    my initials in ascii art style

---
+++ initials.py
@@ -0,0 +1,15 @@
+# My name is Mariia Nebesnaia.
+# Yes, I have two i-s in my first name,
+# because this is a legal translation from my language.
+
+my_initials = """
+M       M N       N
+M M   M M N N     N
+M M   M M N   N   N
+M   M   M N     N N
+M       M N       N
+M       M N       N
+M       M N       N
+"""
+
+print(my_initials)
