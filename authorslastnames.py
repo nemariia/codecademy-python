c7f36c4053eeb1b5defeaf09afc7471a5e553c08
nemariia <lusuwey@gmail.com>
2022-08-05 21:41:01 +0300

    getting last names from the string of names

---
+++ authorslastnames.py
@@ -0,0 +1,6 @@
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(",")
+author_last_names = [n.split()[-1] for n in author_names]
+
+print(author_last_names)
